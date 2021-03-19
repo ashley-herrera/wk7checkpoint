9481d53c85b3de054de974adc3def1001e56bca9
Ashley Herrera <ashleyherrera131@gmail.com>
2021-03-19 15:24:20 -0600

    schemas linked

--- buglog.server/server/db/DbContext.js
+++ buglog.server/server/db/DbContext.js
@@ -1,8 +1,12 @@
 import mongoose from 'mongoose'
 import AccountSchema from '../models/Account'
+import BugSchema from '../models/Bug'
+import NoteSchema from '../models/Note'
 
 class DbContext {
   Account = mongoose.model('Account', AccountSchema);
+  Bug = mongoose.model('Bug', BugSchema)
+  Note = mongoose.model('Note', NoteSchema)
 }
 
 export const dbContext = new DbContext()
