eb6f2081d70bafff3a26f1303c100b7149b894ac
Ashley Herrera <ashleyherrera131@gmail.com>
2021-03-19 15:02:08 -0600

    models

---
+++ buglog.server/server/models/Note.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+const Note = new Schema(
+  {
+    body: { type: String, required: true },
+    bugId: { type: Object, ref: 'Bug', required: true }, // type: ObjectId?
+    creatorId: { type: String, required: true }
+  },
+
+  { timestamps: true, toJSON: { virtuals: true } }
+)
+
+export default Note
